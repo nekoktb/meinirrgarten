8a8c0e04be937513fc6552321137f0441fcccbb2
nekoktb <carlosrogo@correo.ugr.es>
2025-02-26 14:53:30 +0100

    TOma ya

---
+++ P1/src/p1/P1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p1;
+
+/**
+ *
+ * @author nekok
+ */
+public class P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Follar veganos es genial, les gusta el sexo anal entre lechugaaaaaaaaas");
+    }
+    
+}
