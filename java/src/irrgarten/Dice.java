f75ac63a6216642d962b451414bec2c2da1e11d1
nekoktb <carlosrogo@correo.ugr.es>
2025-03-01 19:03:29 +0100

    Dice

--- java/src/irrgarten/Dice.java
+++ java/src/irrgarten/Dice.java
@@ -4,10 +4,27 @@
  */
 package irrgarten;
 
+import java.util.Random;
+
+
 /**
  *
  * @author nekok
  */
 public class Dice {
+    private static final int MAX_USES = 5; //número máximo de usos de armas y escudos
+    private static final double MAX_INTELLIGENCE = 10.0; //valor máximo para la inteligencia de jugadores y monstruos
+    private static final double MAX_STRENGTH = 10.0; //valor máximo para la fuerza de jugadores y monstruos
+    private static final double RESURRECT_PROB = 0.3; //probabilidad de que un jugador sea resucitado en cada turno
+    private static final int WEAPONS_REWARD = 2; //numero máximo de armas recibidas al ganar un combate
+    private static final int SHIELDS_REWARD = 3; //numero máximo de escudos recibidos al ganar un combate
+    private static final int HEALTH_REWARD = 5; //numero máximo de unidades de salud recibidas al ganar un combate
+    private static final int MAX_ATTACK = 3; //máxima potencia de las armas
+    private static final int MAX_SHIELD = 2; //máxima potencia de los escudos
+    
+    private static final Random generator = new Random(); 
+    
+    
+    
     
 }
