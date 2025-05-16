036d136b198bf38e942da6dfc808b45c9c2ab496
nekoktb <carlosrogo@correo.ugr.es>
2025-05-16 14:20:21 +0200

    Pussy acabado

--- java/src/irrgarten/FuzzyPlayer.java
+++ java/src/irrgarten/FuzzyPlayer.java
@@ -5,7 +5,7 @@ public class FuzzyPlayer extends Player {
    
     //Constructor:
     public FuzzyPlayer(Player other) {
-        super(other); //falta el constructor de copia de player 
+        super(other);
     }
 
     public Directions move(Directions direction, ArrayList<Directions> validMoves) {
@@ -14,17 +14,18 @@ public class FuzzyPlayer extends Player {
 
     }
 
-    public void attack() {
-        
+    public float attack() {
+        return (sumWeapons() + Dice.intensity(getStrength()));
     }
 
     protected float defensiveEnergy() {
+        return (sumShields() + Dice.intensity(getIntelligence()));
         
     }
 
     @Override
-    String toString(){
-
+    public String toString(){
+        return ("Fuzzy"+super.toString());
     }
     
 }
